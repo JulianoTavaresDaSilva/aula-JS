5473919f6d039ea82fafe7d60a9146d3dc385cad
JulianoTavaresDaSilva <julianotavares06@gmail.com>
2025-12-08 20:16:56 -0300

    setas para cima e para baixo

--- js/script.js
+++ js/script.js
@@ -1,7 +1,79 @@
+//variáveis globais
 var gridSizeX = 10;
 var gridSizeY = 10;
 
-document.addEventListener('DOMContentLoaded', criaGrid);
+var posicaoX = 0
+var posicaoY = 0
+
+var vidas = 5
+
+//resgistro de eventos
+document.addEventListener('DOMContentLoaded', inicializaJogo);
+document.addEventListener('keydown', processaTecla)
+
+
+//funções
+
+function removeVida(){
+    vidas--;
+
+    if(vidas == 0){
+        inicializaJogo();
+    }else{
+        atualizaVidas();
+    }
+}
+
+function processaTecla(evento){
+    let posicaoAtual = document.querySelector('#bloco'+posicaoX+posicaoY);
+
+    if(evento.key == 'ArrowDown'){
+        let novaPosicao = document.querySelector('#bloco'+(posicaoX + 1) + posicaoY)
+        if(posicaoX < (gridSizeX -1)){
+            posicaoAtual.classList.remove('azul');
+            posicaoAtual.classList.add('cinza')
+            novaPosicao.classList.remove('cinza');
+            novaPosicao.classList.add('azul')
+            posicaoX++
+        }else{
+            posicaoAtual.classList.remove('azul')
+            posicaoAtual.classList.add('vermelho')
+        }
+
+    }
+    
+    if(evento.key == 'ArrowUp'){
+        if(posicaoX < (gridSizeX - 1)){
+            let novaPosicao = document.querySelector('#bloco'+(posicaoX - 1) + posicaoY)
+            posicaoAtual.classList.remove('azul');
+            posicaoAtual.classList.add('cinza')
+            novaPosicao.classList.remove('cinza');
+            novaPosicao.classList.add('azul')
+            posicaoX--
+        }else{
+            posicaoAtual.classList.remove('azul')
+            posicaoAtual.classList.add('vermelho')
+        }
+
+    }
+
+}
+
+function atualizaVidas(){
+    let spanVidas = document.querySelector('#lifes');
+    spanVidas.innerText = vidas;
+}
+
+function inicializaJogo(){
+    criaGrid();
+
+    //Cria personagem
+    let jogador = document.querySelector('#bloco'+posicaoX+posicaoY);
+    jogador.classList.remove('cinza');
+    jogador.classList.add('azul');
+
+    atualizaVidas();
+}
 
 
 function criaGrid() {
